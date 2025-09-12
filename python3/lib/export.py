db425b5a7c40574e68f47b117a1f5d7d361a1a5a
TK <tilkoke@live.de>
2025-09-12 15:40:59 +0200

    Add rough export for all animations Grouping in different nodes not correct Blender does not display any animation, but it list them int eh object tree (other GLTF players seems to work better)

--- python3/lib/export.py
+++ python3/lib/export.py
@@ -29,58 +29,94 @@ def export_to_gltf(model: Model, name: str, output_path: str):
     vertex_byte_array = bytearray()
     uv_byte_array = bytearray()
     index_byte_array = bytearray()
-    
-    for [node_name, animations] in model.objects.items():
+
+    animation_in_byte_array = bytearray()
+    animation_out_byte_array = bytearray()
+    gltf_animations = []
+
+    sampler_idx = 0
+
+    for [node_name, animation_idxs] in model.objects.items():
         kf_meshes = []
         base_node = Node(name=node_name, children=[])
         base_node_idx = len(nodes)
         nodes.append(base_node)
         object_root_nodes.append(base_node_idx)
-        # Invert structure from "list of frames, with list of all meshes" to "list of meshes with list of its frames"
-        meshes_with_frames = [[model.keyframes[i].meshes[j] for i in range(len(model.keyframes))] for j in range(len(model.keyframes[0].meshes))]
-        #meshes_with_frames = list(map(list, zip(*model.keyframes)))
-        keyframe_len = len(meshes_with_frames)
-        for mesh in meshes_with_frames:
-            base_vertices = None
-            base_indices = None
-            base_mesh = None
-            # Load first keyframe as base meshes, rest as morph target
-            for frame in mesh:
-                vertices = [transform_vertex(p) for p in model.vertex_data[frame.vertices]]
-                if base_vertices == None:   base_vertices = vertices
-                else:                       vertices = [vertex - base_vertex for vertex, base_vertex in zip(vertices, base_vertices)]
-                vertex_data_start = len(vertex_byte_array)
-                [vertex_byte_array.extend(struct.pack('fff', v.x, v.y, v.z)) for v in vertices]
-                mins = list(map(min, zip(*vertices)))
-                maxs = list(map(max, zip(*vertices)))
-                vertex_accessor_idx = len(accessors)
-                accessors.append(Accessor(bufferView=0, byteOffset=vertex_data_start, componentType=ComponentType.FLOAT.value, count=len(vertices),
-                                    type=AccessorType.VEC3.value, min=mins, max=maxs))
-                
-                texture_coordinates = model.texture_coordinates_data[frame.texture_coordinates]
-                texture_coords_start = len(uv_byte_array)
-                [uv_byte_array.extend(struct.pack('ff', uv.x, uv.y)) for uv in texture_coordinates]
-                texture_coords_accessors_index = len(accessors)
-                accessors.append(Accessor(bufferView=1, byteOffset=texture_coords_start, componentType=ComponentType.FLOAT.value, count=len(texture_coordinates),
-                                    type=AccessorType.VEC2.value))
-                
-                if base_indices == None:
-                    base_indices = model.triangle_data[frame.triangles]
-                    indices_start = len(index_byte_array)
-                    [index_byte_array.extend(struct.pack('I', index)) for index in base_indices]  
-                    indices_accessor_index = len(accessors)
-                    accessors.append(Accessor(bufferView=2, byteOffset=indices_start, componentType=ComponentType.UNSIGNED_INT.value, count=len(base_indices),
-                                        type=AccessorType.SCALAR.value))
+        for animation_idx in animation_idxs:
+            animation = model.animations[animation_idx]
+            keyframes_in_animation = [kf for index, kf in enumerate(model.keyframes) if index in animation.keyframes]
+            # Invert structure from "list of frames, with list of all meshes" to "list of meshes with list of its frames"
+            meshes_with_frames = [[keyframes_in_animation[i].meshes[j] for i in range(len(keyframes_in_animation))] for j in range(len(keyframes_in_animation[0].meshes))]
+            #meshes_with_frames = list(map(list, zip(*model.keyframes)))
+            for frames in meshes_with_frames:            
+                keyframe_len = len(frames)
+                base_vertices = None
+                base_indices = None
+                base_mesh = None
+                # Load first keyframe as base meshes, rest as morph target
+                for frame in frames:
+                    vertices = [transform_vertex(p) for p in model.vertex_data[frame.vertices]]
+                    if base_vertices == None:   base_vertices = vertices
+                    else:                       vertices = [vertex - base_vertex for vertex, base_vertex in zip(vertices, base_vertices)]
+                    mins = list(map(min, zip(*vertices)))
+                    maxs = list(map(max, zip(*vertices)))
+                    vertex_data_start = len(vertex_byte_array)
+                    [vertex_byte_array.extend(struct.pack('fff', v.x, v.y, v.z)) for v in vertices]
+                    vertex_accessor_idx = len(accessors)
+                    accessors.append(Accessor(bufferView=0, byteOffset=vertex_data_start, componentType=ComponentType.FLOAT.value, count=len(vertices),
+                                        type=AccessorType.VEC3.value, min=mins, max=maxs))
+                    
+                    texture_coordinates = model.texture_coordinates_data[frame.texture_coordinates]
+                    texture_coords_start = len(uv_byte_array)
+                    [uv_byte_array.extend(struct.pack('ff', uv.x, uv.y)) for uv in texture_coordinates]
+                    texture_coords_accessors_index = len(accessors)
+                    accessors.append(Accessor(bufferView=1, byteOffset=texture_coords_start, componentType=ComponentType.FLOAT.value, count=len(texture_coordinates),
+                                        type=AccessorType.VEC2.value))
+                    
+                    if base_indices == None:
+                        base_indices = model.triangle_data[frame.triangles]
+                        indices_start = len(index_byte_array)
+                        [index_byte_array.extend(struct.pack('I', index)) for index in base_indices]  
+                        indices_accessor_index = len(accessors)
+                        accessors.append(Accessor(bufferView=2, byteOffset=indices_start, componentType=ComponentType.UNSIGNED_INT.value, count=len(base_indices),
+                                            type=AccessorType.SCALAR.value))
 
-                if base_mesh == None:
-                    mesh_index = len(kf_meshes)
-                    base_mesh = Mesh(primitives=[Primitive(attributes=Attributes(POSITION=vertex_accessor_idx, TEXCOORD_0=texture_coords_accessors_index), indices=indices_accessor_index, material=frame.material, targets=[])])
-                    kf_meshes.append(base_mesh)
-                    mesh_node_idx = len(nodes)
-                    nodes.append(Node(name=node_name, mesh=mesh_index))
-                    base_node.children.append(mesh_node_idx)
-                else:                
-                    base_mesh.primitives[0].targets.append(Attributes(POSITION=vertex_accessor_idx, TEXCOORD_0=texture_coords_accessors_index))
+                    if base_mesh == None:
+                        mesh_index = len(kf_meshes)
+                        base_mesh = Mesh(primitives=[Primitive(attributes=Attributes(POSITION=vertex_accessor_idx, TEXCOORD_0=texture_coords_accessors_index), indices=indices_accessor_index, material=frame.material, targets=[])])
+                        kf_meshes.append(base_mesh)
+                        mesh_node_idx = len(nodes)
+                        nodes.append(Node(name=node_name, mesh=mesh_index))
+                        base_node.children.append(mesh_node_idx)
+                    else:                
+                        base_mesh.primitives[0].targets.append(Attributes(POSITION=vertex_accessor_idx, TEXCOORD_0=texture_coords_accessors_index))
+            
+                # TODO: Method to get min & max is a lazy hack right now
+                ain_min = 1000000
+                ain_max = 0
+                a_in_byteOffset = len(animation_in_byte_array)
+                a_out_byteOffset = len(animation_out_byte_array)
+                for i in range(1, keyframe_len+1):
+                    ain_val = (i/float(keyframe_len)) * 50
+                    if ain_val < ain_min: ain_min = ain_val
+                    if ain_val > ain_max: ain_max = ain_val
+                    animation_in_byte_array.extend(struct.pack('f', ain_val))
+                    for j in range(1, keyframe_len+1):
+                        if i==(j+1):
+                            animation_out_byte_array.extend(struct.pack('f', 1.0))
+                        else:
+                            animation_out_byte_array.extend(struct.pack('f', 0.0))
+                accessor_a_in_idx = len(accessors)
+                accessors.append(Accessor(bufferView=3, byteOffset=a_in_byteOffset, componentType=ComponentType.FLOAT.value, count=keyframe_len,
+                                    type=AccessorType.SCALAR.value, min=[ain_min], max=[ain_max]))
+                accessor_a_out_idx = len(accessors)
+                accessors.append(Accessor(bufferView=4, byteOffset=a_out_byteOffset, componentType=ComponentType.FLOAT.value, count=keyframe_len*(keyframe_len-1),
+                                    type=AccessorType.SCALAR.value))
+                gltf_anim = Animation(name=animation.name,
+                                channels=[Channel(sampler=0,target=Target(node=len(nodes)-1, path="weights"))],  # TODO: not good, but latest node should be the one that is targeted by this animation?
+                                samplers=[AnimationSampler(input=accessor_a_in_idx, output=accessor_a_out_idx)])
+                gltf_animations.append(gltf_anim)
+
 
     for material in model.materials:
         texture_name = material.name
@@ -106,35 +142,6 @@ def export_to_gltf(model: Model, name: str, output_path: str):
                 materials.append(gltf_material)
                 break
 
-    animation_in_byte_array = bytearray()
-    animation_out_byte_array = bytearray()
-    ain_min = 10000
-    ain_max = 0
-    for i in range(1, keyframe_len+1):
-        ain_val = (i/float(keyframe_len)) * 50
-        if ain_val < ain_min: ain_min = ain_val
-        if ain_val > ain_max: ain_max = ain_val
-        animation_in_byte_array.extend(struct.pack('f', ain_val))
-        for j in range(1, keyframe_len+1):
-            if i==(j+1):
-                animation_out_byte_array.extend(struct.pack('f', 1.0))
-            else:
-                animation_out_byte_array.extend(struct.pack('f', 0.0))
-    accessor_a_in_idx = len(accessors)
-    ain_mins = []
-    ain_maxs = []
-    ain_mins.append(ain_min)
-    ain_maxs.append(ain_max)
-    accessors.append(Accessor(bufferView=3, byteOffset=0, componentType=ComponentType.FLOAT.value, count=keyframe_len,
-                        type=AccessorType.SCALAR.value, min=ain_mins, max=ain_maxs))
-    accessor_a_out_idx = len(accessors)
-    accessors.append(Accessor(bufferView=4, byteOffset=0, componentType=ComponentType.FLOAT.value, count=keyframe_len*keyframe_len,
-                        type=AccessorType.SCALAR.value))
-    anim = Animation(name="test",
-                    channels=[Channel(sampler=0,target=Target(node=0, path="weights"))], 
-                    samplers=[AnimationSampler(input=accessor_a_in_idx, output=accessor_a_out_idx)])
-    animations = [anim]
-
     model = GLTFModel(
         asset=Asset(version='2.0'),
         scenes=[Scene(nodes=[idx for idx, _ in enumerate(object_root_nodes)])],
@@ -155,7 +162,7 @@ def export_to_gltf(model: Model, name: str, output_path: str):
         samplers=gltfsamplers,
         textures=gltftextures,
         images=images,
-        animations=animations
+        animations=gltf_animations
     )
 
 
